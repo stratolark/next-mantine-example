e45ab12c801fdf4e0ff0cc163f3ab915e46e847d
stratolark <69923983+stratolark@users.noreply.github.com>
2023-03-18 20:10:51 -0300

    feat: add mantine feat: add tailwind feat: add @tabler/icons-react

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,34 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import Head from "next/head";
+import { createEmotionCache, MantineProvider } from "@mantine/core";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export const myCache = createEmotionCache({ key: "mantine" });
+
+export default function App(props: AppProps) {
+  const { Component, pageProps } = props;
+
+  return (
+    <>
+      <Head>
+        <title>Page title</title>
+        <meta
+          name='viewport'
+          content='minimum-scale=1, initial-scale=1, width=device-width'
+        />
+      </Head>
+
+      <MantineProvider
+        withGlobalStyles
+        withNormalizeCSS
+        emotionCache={myCache}
+        // theme={{
+        //   /** Put your mantine theme override here */
+        //   colorScheme: "light",
+        // }}
+      >
+        <Component {...pageProps} />
+      </MantineProvider>
+    </>
+  );
 }
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,23 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import Document, { DocumentContext } from "next/document";
+import { ServerStyles, createStylesServer } from "@mantine/next";
+import { myCache } from "./_app";
 
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+const stylesServer = createStylesServer(myCache);
+
+export default class _Document extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const initialProps = await Document.getInitialProps(ctx);
+
+    return {
+      ...initialProps,
+      styles: [
+        initialProps.styles,
+        <ServerStyles
+          html={initialProps.html}
+          server={stylesServer}
+          key='styles'
+        />,
+      ],
+    };
+  }
 }
